caf4163c6726826cdc5fea76366d5e8e0a72044a
Jose Rivera <jriveramerlatx@gmail.com>
2024-09-04 00:52:59 -0600

    Events Handler added

--- Handlers/Categories.py
+++
@@ -1,3 +0,0 @@
-from collections import namedtuple
-
-Category = namedtuple("Category", "name description")
---
+++ test_Events.py
@@ -0,0 +1,22 @@
+import pytest
+from pydantic import ValidationError
+from Events import Category
+
+
+def test_category_ok():
+    """Tests that the Category class is initialized correctly."""
+
+    category_name = "Fun"
+    category = Category(name=category_name)
+
+    assert (
+        category.name == category_name
+    )  # Assert that the name attribute is set correctly
+
+
+def test_category_no_ok():
+    """Tests that the Category class fails for wrong data."""
+
+    invalid_name = None  # Example of an invalid name (None)
+    with pytest.raises(ValidationError):
+        category = Category(name=invalid_name)
